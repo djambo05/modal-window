b2b73c777a1b4730ff02d659d45028527d3f2816
Djamalutin <fisher.gerryy@mail.ru>
2023-05-09 02:20:17 +0300

    feature: css overlay animated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <img />
+          <img
+            src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+            alt="gif"
+          />
         </div>
       </div>
     </div>
